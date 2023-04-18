e73fff466ecad437d65378fc6060a6b521f7aa44
GianlucaPtrn <200188@site.asty-moulin.be>
2023-04-18 15:41:40 +0200

    update files responsive

---
+++ forms/selectMonthForms.php
@@ -0,0 +1,17 @@
+<form action="" method="post" class="formSelectMonth">
+    <select class="FormRetard form-select form-select-md" name="selectMonth">
+        <option selected value="none">Choisissez un mois :</option>
+        <option value="Janvier">Janvier</option>
+        <option value="Fevrier">Fevrier</option>
+        <option value="mars">mars</option>
+        <option value="avril">avril</option>
+        <option value="mai">mai</option>
+        <option value="juin">juin</option>
+        <option value="juillet">juillet</option>
+        <option value="aout">aout</option>
+        <option value="septembre">septembre</option>
+        <option value="octobre">octobre</option>
+        <option value="novembre">novembre</option>
+        <option value="decembre">decembre</option>
+    </select>
+</form>
